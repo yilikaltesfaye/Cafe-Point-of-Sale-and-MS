c367a8bf43deba56196f2f9a1247042ee199f2eb
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2026-07-23 16:49:44 +0300

    feat(prisma): refactor application structure to integrate Prisma service and module

---
+++ cafe-management/src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+import 'dotenv/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: `${process.env.DATABASE_URL}`,
+    });
+    super({ adapter });
+  }
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
