8b333d51f103ab344d22de91a543b6d32c07e76e
Yilikal Tesfaye <yilikalt.kebede@gmail.com>
2026-07-28 14:41:32 +0300

    feat(frontend): initialized the frontend with vite and installed libraries crucial to the development enviroment

---
+++ cafeposMS/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			kjkj
+			<h1>Yohana Abenezer</h1>
+			<h1>Yilikal Tesfaye</h1>
+			<h1>Yahleel Abenezer</h1>
+			<h1>Tsion Tesfaye</h1>
+		</>
+	);
+}
+
+export default App;
